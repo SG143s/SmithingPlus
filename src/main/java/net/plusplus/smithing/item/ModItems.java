8370e79fe4900d1b91e904fcaa9fde118f200618
SG143s <ariqsumadewa@gmail.com>
2024-01-07 01:51:37 +0800

    Added all unrefined diamond tools

--- src/main/java/net/plusplus/smithing/item/ModItems.java
+++ src/main/java/net/plusplus/smithing/item/ModItems.java
@@ -4,10 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -109,7 +106,15 @@ public class ModItems {
 
 
     public static final Item UNREFINED_DIAMOND_PICKAXE = registerItem("unrefined_diamond_pickaxe",
-            new PickaxeItem(ModToolMaterial.UNREFINED_DIAMOND, 1, -2.8f, new FabricItemSettings()));
+            new PickaxeItem(ModToolMaterial.UNREFINED_DIAMOND, 0, -2.8f, new FabricItemSettings()));
+    public static final Item UNREFINED_DIAMOND_SWORD = registerItem("unrefined_diamond_sword",
+            new SwordItem(ModToolMaterial.UNREFINED_DIAMOND, 2, -2.4f, new FabricItemSettings()));
+    public static final Item UNREFINED_DIAMOND_AXE = registerItem("unrefined_diamond_axe",
+            new AxeItem(ModToolMaterial.UNREFINED_DIAMOND, 5.25f, -3.1f, new FabricItemSettings()));
+    public static final Item UNREFINED_DIAMOND_SHOVEL = registerItem("unrefined_diamond_shovel",
+            new ShovelItem(ModToolMaterial.UNREFINED_DIAMOND, 0.5f, -3f, new FabricItemSettings()));
+    public static final Item UNREFINED_DIAMOND_HOE = registerItem("unrefined_diamond_hoe",
+            new HoeItem(ModToolMaterial.UNREFINED_DIAMOND, -3, -1f, new FabricItemSettings()));
 
     public static final Item UNREFINED_IRON_PICKAXE = registerItem("unrefined_iron_pickaxe",
             new PickaxeItem(ModToolMaterial.UNREFINED_IRON, 1, -2.8f, new FabricItemSettings()));
@@ -126,10 +131,17 @@ public class ModItems {
     }
     private static void addToolMenu (FabricItemGroupEntries entries) {
         entries.add(UNREFINED_DIAMOND_PICKAXE);
+        entries.add(UNREFINED_DIAMOND_AXE);
+        entries.add(UNREFINED_DIAMOND_SHOVEL);
+        entries.add(UNREFINED_DIAMOND_HOE);
 
         entries.add(UNREFINED_GOLDEN_PICKAXE);
 
         entries.add(UNREFINED_IRON_PICKAXE);
+    }
+
+    private static void addCombatMenu (FabricItemGroupEntries entries) {
+        entries.add(UNREFINED_DIAMOND_SWORD);
     }
 
     private static Item registerItem(String name, Item item) {
@@ -140,5 +152,6 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addIngredientMenu);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(ModItems::addToolMenu);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ModItems::addCombatMenu);
     }
 }
