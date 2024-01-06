01cb31edaee4894e08d275b14f0e087479a98732
SG143s <ariqsumadewa@gmail.com>
2024-01-07 00:55:09 +0800

    Add Tooltips to Refining Templates

--- src/main/java/net/plusplus/smithing/item/ModItems.java
+++ src/main/java/net/plusplus/smithing/item/ModItems.java
@@ -3,21 +3,109 @@ package net.plusplus.smithing.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 import net.plusplus.smithing.SmithingPlus;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+class PickaxeRefiningTemplate extends Item {
+
+    public PickaxeRefiningTemplate(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.applyState"));
+        tooltip.add(Text.translatable("tooltip.smithingplus.pickaxe_refining_template.applyOption").formatted(Formatting.BLUE));
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.ingredientState"));
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.ingredientOption").formatted(Formatting.BLUE));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
+
+class SwordRefiningTemplate extends Item {
+
+    public SwordRefiningTemplate(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.applyState"));
+        tooltip.add(Text.translatable("tooltip.smithingplus.sword_refining_template.applyOption").formatted(Formatting.BLUE));
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.ingredientState"));
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.ingredientOption").formatted(Formatting.BLUE));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
+
+class AxeRefiningTemplate extends Item {
+
+    public AxeRefiningTemplate(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.applyState"));
+        tooltip.add(Text.translatable("tooltip.smithingplus.axe_refining_template.applyOption").formatted(Formatting.BLUE));
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.ingredientState"));
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.ingredientOption").formatted(Formatting.BLUE));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
+
+class ShovelRefiningTemplate extends Item {
+
+    public ShovelRefiningTemplate(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.applyState"));
+        tooltip.add(Text.translatable("tooltip.smithingplus.shovel_refining_template.applyOption").formatted(Formatting.BLUE));
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.ingredientState"));
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.ingredientOption").formatted(Formatting.BLUE));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
+
+class HoeRefiningTemplate extends Item {
+
+    public HoeRefiningTemplate(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.applyState"));
+        tooltip.add(Text.translatable("tooltip.smithingplus.hoe_refining_template.applyOption").formatted(Formatting.BLUE));
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.ingredientState"));
+        tooltip.add(Text.translatable("tooltip.smithingplus.refining_template.ingredientOption").formatted(Formatting.BLUE));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
 
 public class ModItems {
 
-    public static final Item PICK_TEMPLATE = registerItem("pickaxe_refining_template", new Item(new FabricItemSettings()));
-    public static final Item AXE_TEMPLATE = registerItem("axe_refining_template", new Item(new FabricItemSettings()));
-    public static final Item SWORD_TEMPLATE = registerItem("sword_refining_template", new Item(new FabricItemSettings()));
-    public static final Item SHOVEL_TEMPLATE = registerItem("shovel_refining_template", new Item(new FabricItemSettings()));
-    public static final Item HOE_TEMPLATE = registerItem("hoe_refining_template", new Item(new FabricItemSettings()));
+    public static final Item PICK_TEMPLATE = registerItem("pickaxe_refining_template", new PickaxeRefiningTemplate(new FabricItemSettings()));
+    public static final Item AXE_TEMPLATE = registerItem("axe_refining_template", new AxeRefiningTemplate(new FabricItemSettings()));
+    public static final Item SWORD_TEMPLATE = registerItem("sword_refining_template", new SwordRefiningTemplate(new FabricItemSettings()));
+    public static final Item SHOVEL_TEMPLATE = registerItem("shovel_refining_template", new ShovelRefiningTemplate(new FabricItemSettings()));
+    public static final Item HOE_TEMPLATE = registerItem("hoe_refining_template", new HoeRefiningTemplate(new FabricItemSettings()));
 
 
     public static final Item UNREFINED_DIAMOND_PICKAXE = registerItem("unrefined_diamond_pickaxe",
