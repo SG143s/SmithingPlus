4e249fd7f0107e338b12caa95e01b1ce4b1fa034
SG143s <ariqsumadewa@gmail.com>
2024-01-08 00:23:45 +0800

    Added trade and recipe

--- src/main/java/net/plusplus/smithing/SmithingPlus.java
+++ src/main/java/net/plusplus/smithing/SmithingPlus.java
@@ -3,6 +3,7 @@ package net.plusplus.smithing;
 import net.fabricmc.api.ModInitializer;
 
 import net.plusplus.smithing.item.ModItems;
+import net.plusplus.smithing.util.CustomTrade;
 import net.plusplus.smithing.util.LootTableModifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,5 +16,6 @@ public class SmithingPlus implements ModInitializer {
 	public void onInitialize() {
 		ModItems.registerModItems();
 		LootTableModifier.ModifyLootTables();
+		CustomTrade.registerTrade();
 	}
 }
