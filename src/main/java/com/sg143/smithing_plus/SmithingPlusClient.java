6f0d84b7d3075bc394162e0f9986b73b8faf69fa
SG143s <ariqsumadewa@gmail.com>
2025-05-28 18:41:18 +0700

    Initial commit

---
+++ src/main/java/com/sg143/smithing_plus/SmithingPlusClient.java
@@ -0,0 +1,10 @@
+package com.sg143.smithing_plus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmithingPlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
