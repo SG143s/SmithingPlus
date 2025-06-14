934e2a6793c47b66f5f978375ac5638c38c4b0d6
SG143s <ariqsumadewa@gmail.com>
2025-06-14 22:18:44 +0700

    Added Unrefined Armor textures and trying loottable

---
+++ src/main/java/com/sg143/smithing_plus/datagen/ModEquipmentAsset.java
@@ -0,0 +1,10 @@
+package com.sg143.smithing_plus.datagen;
+
+import net.minecraft.client.data.EquipmentAssetProvider;
+import net.minecraft.data.DataOutput;
+
+public class ModEquipmentAsset extends EquipmentAssetProvider {
+    public ModEquipmentAsset(DataOutput output) {
+        super(output);
+    }
+}
